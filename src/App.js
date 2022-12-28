8d0e29905c2f58af9a3b68749503a66a0a72f99d
ameya-6964 <belvalkar7@gmail.com>
2022-12-29 01:35:17 +0530

    Added Emoji

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import CreateTweet from './components/CreateTweet';
 import TweetList from './components/TweetList';
 
 function App() { 
-  const name = "Ameya Belvalkar";
+  const name = "Ameya";
   const [textInput,setTextInput] = useState("");
   const [tweets,setTweets] = useState([]); 
-  //const message = "This Is Demo String Message To Check My React";
   return (
   <div className='App'>
-    <h1> Hello {name} </h1>
+    <h1> Welcome {name} Start Tweeting 🔥</h1>
     <CreateTweet textInput={textInput} setTextInput={setTextInput} tweets={tweets} setTweets={setTweets} />
     <TweetList name={name} tweets={tweets} setTweets={setTweets} />
   </div>
